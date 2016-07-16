201b7ab57f3edeb149000a600fad0a2aa8a409ca
Taylor <fahlmantaylor@gmail.com>
2016-07-16 16:26:16 -0700

    Comment block to list option, implement version and help options

--- src/main.rs
+++ src/main.rs
@@ -5,4 +5,37 @@ use std::env;
 fn main() {
 
     let args: Vec<String> = env::args().collect();
+    let program = args[0].clone();
+
+    let mut opts = Options::new();
+    /*Options available
+     * -a archive file(s) specified
+     * -e extract file(s) from archive specified
+     * -d Remove files from archive
+     * -o overwrite files when extracting (e must be specified)
+     * -h Displays help
+     * -t Short table of contents for archive
+     * -T Long table of contents for archive
+     * -V version info
+     * -v verbose mode
+    */
+
+    //Opt Flag Options
+    opts.optflag("V", "Version", "Version 0.1.0");
+    opts.optflag("h", "help", "Prints this help menu");
+
+    //Opt opt Options
+
+    //Block to ensure the option exists
+    let matches = match opts.parse(&args[1..]) {
+        Ok(m) => { m }
+        Err(f) => { panic!(f.to_string()) }
+    };
+
+    if matches.opt_present("V") {
+        let brief = format!("Version 0.1.0");
+        println!("{}", brief);
+        return;
+    }
+
 }
