cdebdb1c131972cd04b801bc1ad92adaa35c4212
Taylor <fahlmantaylor@gmail.com>
2016-07-16 12:19:02 -0700

    Collect args into a string vector

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 extern crate getopts;
+use getopts::Options;
 use std::env;
 
 fn main() {
-    if let Some(arg1) = env::args().nth(1) {
-        println!("The first argument is {}", arg1);
-    }
+
+    let args: Vec<String> = env::args().collect();
 }
